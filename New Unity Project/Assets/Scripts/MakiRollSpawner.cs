c40757f073fc986e7d9055cbc563720909bde7a1
EMYGP\EmyGP <emyburgp@gmail.com>
2020-06-18 12:32:01 +0200

    aggiunti spawner, da finire, e vassoi

---
+++ New Unity Project/Assets/Scripts/MakiRollSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakiRollSpawner : MonoBehaviour
+{
+    public GameObject makiRoll1;
+    public Vector3 spawnPoint;
+
+    void Start()
+    {
+        Instantiate(makiRoll1, spawnPoint, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
