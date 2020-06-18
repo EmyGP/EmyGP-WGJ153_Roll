fba5e015b5478cb9a729825dc4cf5e1d916131e4
EMYGP\EmyGP <emyburgp@gmail.com>
2020-06-18 19:20:48 +0200

    aggiunta grafica, modificati alcuni prefab

--- New Unity Project/Assets/Scripts/MakiRollSpawner2.cs
+++ New Unity Project/Assets/Scripts/MakiRollSpawner2.cs
@@ -5,23 +5,39 @@ using UnityEngine;
 public class MakiRollSpawner2 : MonoBehaviour
 {
     public GameObject makiRoll1;
-    public int timeIntervals;
+        //ogni quanto spawnano
+    public float timeIntervals;
+        //quanti spawn voglio avere
+    public float maxSpawn;
     public int x;
     public int y;
+    public GameObject[] makiRolls;
+    private int makiRollsN;
 
-    void Start() { }
-
-    IEnumerator SpawnTimer()
+    void Awake()
     {
-        for (; ; )
-        {
-            Spawning();
-            yield return new WaitForSeconds(timeIntervals);
-        }
+        //mT = timeIntervals * maxSpawn;
+        makiRolls = GameObject.FindGameObjectsWithTag("Makiroll");
+        int makiRollsN = GameObject.FindGameObjectsWithTag("Makiroll").Length;
+        InvokeRepeating("Spawning", maxSpawn, timeIntervals);
+    }
+
+    private void Start()
+    {
     }
 
     void Spawning()
+    {    
+        Instantiate(makiRolls[makiRollsN], new Vector3(x, y, 0), Quaternion.identity);
+        Debug.Log("ehiiiiiiiiiiiiiiiii");
+    }
+
+    private void Update()
     {
-         Instantiate(makiRoll1, new Vector3(x, y, 0), Quaternion.identity);
+        if (makiRollsN <= 0)
+        {
+            //ferma lo spawn dei maki rolls
+        }
+        
     }
 }
