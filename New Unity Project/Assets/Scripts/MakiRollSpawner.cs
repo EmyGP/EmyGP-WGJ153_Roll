360f97601298d725b2e81b9a1f9639186b3808ad
EMYGP\EmyGP <emyburgp@gmail.com>
2020-06-18 15:00:41 +0200

    roba

--- New Unity Project/Assets/Scripts/MakiRollSpawner.cs
+++ New Unity Project/Assets/Scripts/MakiRollSpawner.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class MakiRollSpawner : MonoBehaviour
 {
     public GameObject makiRoll1;
-    public Vector3 spawnPoint;
+    public int widthOffset;
+    public int heightOffset;
+    public int width;
+    public int height;
 
     void Start()
     {
-        Instantiate(makiRoll1, spawnPoint, Quaternion.identity);
+        for (int y=0; y<height; ++y)
+        {
+            for(int x=0; x<width; ++x)
+            {
+                Instantiate(makiRoll1, new Vector3(x+widthOffset, y+heightOffset, 0), Quaternion.identity);
+            }
+        }
+
     }
 
     // Update is called once per frame
---
+++ New Unity Project/Assets/Scripts/MakiRollSpawner2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakiRollSpawner2 : MonoBehaviour
+{
+    public GameObject makiRoll1;
+    public int timeIntervals;
+    public int x;
+    public int y;
+
+    void Start() { }
+
+    IEnumerator SpawnTimer()
+    {
+        for (; ; )
+        {
+            Spawning();
+            yield return new WaitForSeconds(timeIntervals);
+        }
+    }
+
+    void Spawning()
+    {
+         Instantiate(makiRoll1, new Vector3(x, y, 0), Quaternion.identity);
+    }
+}
